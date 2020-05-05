32eab020393b3f73debdf6d445b5297ef6f0f974
Andrea Ramirez <anlramirezs11@gmail.com>
2020-05-05 11:35:30 -0500

    Se crean todas las rutas para el proyecto

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => (
+    <React.Fragment>
+        <h1>404</h1>
+        <h2>Página no encontrada</h2>
+        <p>Regresa a la pagina Anterior</p>
+    </React.Fragment>
+
+);
+
+export default NotFound;
