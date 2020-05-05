61bae57d1b5a0e82ac18b5b3c3daf988cfb5672b
Andrea Ramirez <anlramirezs11@gmail.com>
2020-05-05 15:44:23 -0500

    Clase 12: Se crea el store de Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import { initalState } from '../initialState.json';
+initalState.user = {};
+initalState.playing = {};
+
+const store = createStore(reducer, initalState);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+);
