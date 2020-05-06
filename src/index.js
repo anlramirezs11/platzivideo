c5e51e31a763da9223c70b8381fa6dc5fca174fc
Andrea Ramirez <anlramirezs11@gmail.com>
2020-05-06 11:50:25 -0500

    Clase 27: Se crea la conección con la extención de Redux DevTools para Chrome o Firefox

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import App from './routes/App';
 import reducer from './reducers';
 
@@ -9,7 +9,10 @@ import { initalState } from '../initialState.json';
 initalState.user = {};
 initalState.playing = {};
 
-const store = createStore(reducer, initalState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(reducer, initalState, composeEnhancers);
 ReactDOM.render(
     <Provider store={store}>
         <App />
